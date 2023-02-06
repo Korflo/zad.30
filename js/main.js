6b59511d8490813dcf0c06b01d9f00898b5eaed8
korneliaflorkowka@gmail.com <106426689+Korflo@users.noreply.github.com>
2023-02-06 16:54:40 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// 1. Stwórz dokument HTML. Stwórz plik main.js w odpowiednim folderze i podepnij go pod plik HTML.
+
+// 2. W pliku html wewnątrz body dodaj pusty paragraf oraz podepnij Twój skrypt main.js.
+
+// 3 . W pliku main.js stworz zmienną o dowolnej nazwie i przypisz do niej stworzony wcześniej paragraf (przy użyciu document.querySelector).
+
+// 4. Następnie dodaj do paragrafu dowolny tekst wykorzystując właściwość textContent
+
+// Pliki dodaj do zdalnego repozytorium i podeślij link do sprawdzenia.
+
+let a = document.querySelector('p').textContent = 'This text is different!';
